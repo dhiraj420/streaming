908cb605928833d7411d28df610e870f0b18fbe9
dhiraj420 <88832108+dhiraj420@users.noreply.github.com>
2021-12-08 21:50:41 +0530

    Update video.php

--- video.php
+++ video.php
@@ -1,6 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Origin: *');
 ?>
 
-
-<!-- sony ten2:- https://pubads.g.doubleclick.net/ssai/event/Syu8F41-R1y_JmQ7x0oNxQ/master.m3u8 -->
