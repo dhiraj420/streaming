64eeb92214ccae67cc9862c7bf416589eae97da2
dhiraj420 <88832108+dhiraj420@users.noreply.github.com>
2021-12-08 21:56:39 +0530

    Update video.php

--- video.php
+++ video.php
@@ -1,32 +1,3 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+echo "dfdg";
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <link href="https://vjs.zencdn.net/7.15.4/video-js.css" rel="stylesheet" />
-  </head>
-
-  <body>
-    <video
-      id="my-video"
-      class="video-js"
-      controls
-      preload="auto"
-      width="640"
-      height="264"
-      data-setup="{}"
-    >
-      <source src="https://pubads.g.doubleclick.net/ssai/event/Syu8F41-R1y_JmQ7x0oNxQ/master.m3u8" type="application/x-mpegURL"  />
-      <p class="vjs-no-js">
-        To view this video please enable JavaScript, and consider upgrading to a
-        web browser that
-        <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-      </p>
-    </video>
-
-    <script src="https://vjs.zencdn.net/7.15.4/video.min.js"></script>
-  </body>
-</html>
-
-<!-- sony ten2:- https://pubads.g.doubleclick.net/ssai/event/Syu8F41-R1y_JmQ7x0oNxQ/master.m3u8 -->
